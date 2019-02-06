4bf7f82e0bf44a2dc2b02a05f653b1da98656fb4
Wesley <we.evenwel@gmail.com>
2019-02-06 21:46:23 +0200

    Added a comment

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -9,10 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var buttonCount = 0
+    @IBOutlet weak var maLabel: UILabel!
+    
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        buttonCount = buttonCount + 1
+        
+        print(buttonCount)
+        /* this is a commut test
+        if buttonCount >= 10 {
+            
+        view.backgroundColor = UIColor.red
+        
+        maLabel.text = "Nick is cool"
+          }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+     
     }
 
 
