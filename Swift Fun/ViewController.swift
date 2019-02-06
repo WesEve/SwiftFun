8f664c3274d82b1143a68cc5ae1ef491c1f8a595
Wesley Evenwel <wesleyevenwel@client.openvpn.net>
2019-02-06 14:39:54 +0200

    Initial Commit

---
+++ Swift Fun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Fun
+//
+//  Created by Wesley Evenwel on 2019/02/06.
+//  Copyright © 2019 Wesley Evenwel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
